10f0cd2454293232fce5275a2d1b58536c1aa43f
Lycanthrope <Taszidizaz@iut-dhaka.edu>
2023-09-03 17:13:36 +0600

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
-import './app.css';
+import './App.css';
 import MainContent from './components/MainContent';
-// import Cursor from './components/MainContent/Cursor';
+import Cursor from './components/Cursor';
 import Navbar from './components/Navbar';
 import Purple from './components/Purple';
 import Footer from './components/Footer';
-
+import { useEffect, useRef } from 'react';
 
 const App = () => {
+  const cursorRef = useRef(null);
+
+  useEffect(() => {
+    const cursor = cursorRef.current;
+
+    document.addEventListener("mousemove", (e) => {
+        cursor.style.left = e.x + 20 + "px";
+        cursor.style.top = e.y + 20 + "px";
+    });
+
+    // Box hover logic
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => {
+        box.addEventListener("mouseenter", function() {
+            const att = box.getAttribute("data-image");
+            cursor.style.width = "470px";
+            cursor.style.height = "370px";
+            cursor.style.borderRadius = "0";
+            cursor.style.backgroundImage = `url(${att})`;
+        });
+        box.addEventListener("mouseleave", function() {
+            box.style.backgroundColor = "transparent";
+            cursor.style.width = "20px";
+            cursor.style.height = "20px";
+            cursor.style.borderRadius = "50%";
+            cursor.style.backgroundImage = `none`;
+        });
+    });
+
+    return () => {
+      // Cleanup any other listeners if needed
+    };
+
+  }, []);
 
   return (
     <>
-      {/* <Cursor ref={cursorRef} /> */}
+      <Cursor ref={cursorRef} />
       <Purple/>
       <Navbar />
       <MainContent />
--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -10,17 +10,17 @@ import Page2 from './Page2';
 import Page3 from './Page3';
 import Page4 from './Page4';
 import Page5 from './Page5';
-import Cursor from './Cursor';
+// import Cursor from '../Cursor';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const MainContent = () => {
     const mainRef = useRef(null);
-    const cursorRef = useRef(null);
+    // const cursorRef = useRef(null);
 
     useEffect(() => {
         const main = mainRef.current;
-        const cursor = cursorRef.current;
+        // const cursor = cursorRef.current;
 
         // Initialization Logic
         const locoScroll = new LocomotiveScroll({
@@ -42,11 +42,11 @@ const MainContent = () => {
         ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
         ScrollTrigger.refresh();
 
-        // Cursor Logic
-        document.addEventListener("mousemove", (e) => {
-            cursor.style.left = e.x + 20 + "px";
-            cursor.style.top = e.y + 20 + "px";
-        });
+        // // Cursor Logic
+        // document.addEventListener("mousemove", (e) => {
+        //     cursor.style.left = e.x + 20 + "px";
+        //     cursor.style.top = e.y + 20 + "px";
+        // });
 
         // GSAP Animations
         gsap.from(".page1 h1,.page1 h2", {
@@ -93,23 +93,23 @@ const MainContent = () => {
         });
         tl3.to(main, { backgroundColor: "#0F0D0D" });
 
-        const boxes = document.querySelectorAll(".box");
-        boxes.forEach((box) => {
-            box.addEventListener("mouseenter", function() {
-                const att = box.getAttribute("data-image");
-                cursor.style.width = "470px";
-                cursor.style.height = "370px";
-                cursor.style.borderRadius = "0";
-                cursor.style.backgroundImage = `url(${att})`;
-            });
-            box.addEventListener("mouseleave", function() {
-                box.style.backgroundColor = "transparent";
-                cursor.style.width = "20px";
-                cursor.style.height = "20px";
-                cursor.style.borderRadius = "50%";
-                cursor.style.backgroundImage = `none`;
-            });
-        });
+        // const boxes = document.querySelectorAll(".box");
+        // boxes.forEach((box) => {
+        //     box.addEventListener("mouseenter", function() {
+        //         const att = box.getAttribute("data-image");
+        //         cursor.style.width = "470px";
+        //         cursor.style.height = "370px";
+        //         cursor.style.borderRadius = "0";
+        //         cursor.style.backgroundImage = `url(${att})`;
+        //     });
+        //     box.addEventListener("mouseleave", function() {
+        //         box.style.backgroundColor = "transparent";
+        //         cursor.style.width = "20px";
+        //         cursor.style.height = "20px";
+        //         cursor.style.borderRadius = "50%";
+        //         cursor.style.backgroundImage = `none`;
+        //     });
+        // });
 
         const h4Elems = document.querySelectorAll("#nav h4");
         const purple = document.querySelector("#purple");
@@ -134,7 +134,7 @@ const MainContent = () => {
 
     return (
         <div className="main" ref={mainRef}>
-            <Cursor ref={cursorRef} />
+            {/* <Cursor ref={cursorRef} /> */}
             <Page1 />
             <Page2 />
             <Page3 />
